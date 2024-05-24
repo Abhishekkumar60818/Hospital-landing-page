a08c07b9d225d4e7aa6e3b4b13c92ef4d077df09
Abhishek Kumar <95571253+Abhishekkumar60818@users.noreply.github.com>
2024-05-24 08:33:49 +0530

    some change

--- src/components/ServicesSection.js
+++ src/components/ServicesSection.js
@@ -7,16 +7,18 @@ import { CiMemoPad } from "react-icons/ci";
 
 const ServicesSection = () => {
   return (
-    <section className="services-section">
-      <h2>Our Services</h2>
-      <p>We provide the most full medical services, so every person could have the opportunity to receive qualitative medical help.</p>
-      <div className="services">
-        <ServiceCard title="Dental Care" icon={<TbDental />} />
-        <ServiceCard title="Pulmonary" icon={<BsLungs />} />
-        <ServiceCard title="Neurological" icon={<PiBrainLight />} />
-        <ServiceCard title="Pediatrics" icon={<CiMemoPad />} />
-      </div>
-    </section>
+    <div className="services-container"> {/* Wrap the section in a container */}
+      <section className="services-section">
+        <h2>Our Services</h2>
+        <p>We provide the most full medical services, so every person could have the opportunity to receive qualitative medical help.</p>
+        <div className="services">
+          <ServiceCard title="Dental Care" icon={<TbDental />} />
+          <ServiceCard title="Pulmonary" icon={<BsLungs />} />
+          <ServiceCard title="Neurological" icon={<PiBrainLight />} />
+          <ServiceCard title="Pediatrics" icon={<CiMemoPad />} />
+        </div>
+      </section>
+    </div>
   );
 };
 
